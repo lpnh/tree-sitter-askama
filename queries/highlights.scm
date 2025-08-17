754d5f1217190e840d86ef586cd1e91126190c34
lpnh <paniguel.lpnh@gmail.com>
2025-08-17 18:59:59 -0300

    improve highlights
    the less the better

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,30 +1,26 @@
-[
-  "{{"
-  "{{-"
-  "{{+"
-  "{{~"
-  "~}}"
-  "+}}"
-  "-}}"
-  "}}"
-  "{%"
-  "{%-"
-  "{%+"
-  "{%~"
-  "~%}"
-  "+%}"
-  "-%}"
-  "%}"
-] @keyword.directive
+; Identifiers
+(identifier) @variable
 
+((identifier) @type
+  (#lua-match? @type "^[A-Z]"))
+
+((identifier) @constant
+  (#lua-match? @constant "^[A-Z][A-Z%d_]*$"))
+
+((identifier) @constant.builtin
+  (#any-of? @constant.builtin "Some" "None" "Ok" "Err"))
+
+((identifier) @module
+  (#any-of? @module "crate" "super" "self"))
+
+; Literals
 (string_literal) @string
 
 (number_literal) @number
 
 (boolean_literal) @boolean
 
-(comment) @comment @spell
-
+; Punctuation
 [
   ","
   "."
@@ -38,14 +34,13 @@
   "]"
 ] @punctuation.bracket
 
+; Operators
 [
   "|"
   "="
 ] @operator
 
 [
-  "is"
-  "is not"
   "bitor"
   "xor"
   "bitand"
@@ -64,40 +59,20 @@
   "!="
 ] @operator
 
-[
-  "block"
-  "filter"
-  "extends"
-  "let"
-  "set"
-  "match"
-  "when"
-  "macro"
-  "call"
-  "import"
-  "as"
-  "with"
-  "defined"
-] @keyword
-
-[
-  "endblock"
-  "endmacro"
-] @keyword
-
-; End statement
-(endfilter_statement) @keyword
-(endmatch_statement) @keyword
-(endwhen_statement) @keyword
-(endcall_statement) @keyword
-
+; Keywords
 [
   "if"
-  "else"
   "else if"
   "elif"
-  "endif"
+  (else_statement)
+  (endif_statement)
 ] @keyword.conditional
+
+[
+  "is"
+  "is not"
+  "defined"
+] @constant.builtin
 
 [
   "for"
@@ -105,82 +80,87 @@
   (endfor_statement)
 ] @keyword.repeat
 
+[
+  "match"
+  (endmatch_statement)
+  "when"
+  "with"
+  (endwhen_statement)
+] @keyword.conditional
+
+[
+  "block"
+  "endblock"
+  "filter"
+  (endfilter_statement)
+  "macro"
+  "endmacro"
+  "call"
+  (endcall_statement)
+] @keyword
+
+[
+  "extends"
+  "include"
+  "import"
+  "as"
+] @keyword.import
+
+[
+  "let"
+  "set"
+  "mut"
+] @keyword
+
+; Delimiters
+[
+  "{%"
+  "{%-"
+  "{%+"
+  "{%~"
+  "%}"
+  "-%}"
+  "+%}"
+  "~%}"
+] @keyword.directive
+
+[
+  "{{"
+  "{{-"
+  "{{+"
+  "{{~"
+  "}}"
+  "-}}"
+  "+}}"
+  "~}}"
+] @keyword.directive
+
+; Field variables
+(field_access_expression
+  field: (identifier) @variable.member)
+
+(field_access_expression
+  field: (number_literal) @variable.member)
 
 ; Function calls
 (call_expression
-  (identifier) @function.call)
-
-(call_expression
-  (path_expression) @function.call)
+  function: (identifier) @function.call)
 
 (call_expression
   (field_access_expression
-    (identifier) @function.call))
+    field: (identifier) @function.call))
 
-; Macro calls
-(macro_call_statement
-  (identifier) @function.call)
+(call_expression
+  (path_expression
+    name: (identifier) @function))
 
 ; Filter names
 (filter
   name: (identifier) @function.builtin)
 
-; Macro definitions
-(macro_statement
-  (identifier) @function.method)
+; Comments
+(comment) @comment @spell
 
-; Block names
-(block_statement
-  (identifier) @tag)
-
-(endblock_statement
-  (identifier) @tag)
-
-; Variables and identifiers
-(let_statement
-  (identifier) @variable)
-
-(let_statement
-  (tuple_pattern
-    (identifier) @variable))
-
-(for_statement
-  (identifier) @variable)
-
-(for_statement
-  (tuple_pattern
-    (identifier) @variable))
-
-; Field access
-(field_access_expression
-  (identifier) @variable
-  (identifier) @property)
-
-; Include and import paths
-(extends_statement
-  (string_literal) @string.special.path)
-
-(include_statement
-  (string_literal) @string.special.path)
-
-(import_statement
-  (string_literal) @string.special.path)
-
-; Import aliases
-(import_statement
-  (identifier) @variable)
-
-; Pattern matching
-(pattern
-  (identifier) @variable)
-
-(destructure_element
-  (identifier) @variable)
-
-; Path expressions (module::function)
-(path_expression
-  (identifier) @module
-  (identifier) @function .)
-
-; Wildcard
-"_" @variable.builtin
+; Wildcards
+"_" @character.special
+".." @character.special
